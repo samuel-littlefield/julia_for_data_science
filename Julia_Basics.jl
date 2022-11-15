8a02a39f09a53bf43297edde0d9f7dd1c759c7ae
samuel-littlefield <s7elijah@gmail.com>
2022-11-14 23:38:38 -0600

    Started second section, which focuses on arrays

--- Julia_Basics.jl
+++ Julia_Basics.jl
@@ -1,3 +1,5 @@
+####### JULIA BASICS #######
+
 ####### Data Types #######
 
 # Assign the value "hello world!" to variable x
@@ -19,3 +21,14 @@ z = Int32(y) # could save variable back to y instead of z.
 
 # 3. Use double colons
 y::Int64
+
+####### Arrays #######
+
+# We can create arrays
+array = [1, 3.14, false, 'j', "some string"]
+
+# We can call items in the array (NOTE: Julia starts at 1 not 0 like Python)
+array[1]
+
+# The last item in an array can be called using [end]
+array[end]
